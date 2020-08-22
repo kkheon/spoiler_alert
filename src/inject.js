9a5b27852dab7816c3adc72c4f89b911accc29ed
kkheon <kkheon@users.noreply.github.com>
2020-08-22 14:59:09 +0900

    Update inject.js
    1. TargetString 분리
    2. title도 확인하도록 수정.
      - text 전체가 red로 변하기 때문에 여기에서는 제외

--- src/inject.js
+++ src/inject.js
@@ -4,20 +4,29 @@ chrome.extension.sendMessage({}, function (response) {
             clearInterval(readyStateCheckInterval);
 
 			var findSpoilers = function (){
-				//var infoSpan = document.querySelectorAll('[class=title_area]');
+				var TargetString = '(테넷|TENET|tenet)';
+				
 				var infoSpan = Array.from(document.getElementsByClassName('title_area'));
+				
 				// notice 
 				infoSpan = infoSpan.concat(Array.from(document.querySelectorAll('[class=notice] > [class="list_left list_title"]')));
 				// popular list
 				infoSpan = infoSpan.concat(Array.from(document.getElementsByClassName('list_main')));
 				for (var i = 0; i < infoSpan.length; i++) {
-					if(infoSpan[i].innerText.match('(테넷)')){
+					if(infoSpan[i].innerText.match(TargetString)){
 						infoSpan[i].prepend('[테넷]');
 						infoSpan[i].style.backgroundColor = '#333';
 						infoSpan[i].style.color = 'red';
 
 					}
-                }
+				}
+				// title 
+				infoSpan = document.querySelectorAll('[class=list_body] > [class="list_title"]');
+				for (var i = 0; i < infoSpan.length; i++) {
+					if(infoSpan[i].innerText.match(TargetString)){
+						infoSpan[i].style.backgroundColor = '#333';
+					}
+				}
 			};
             findSpoilers();
         }
